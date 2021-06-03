82459d14150ae9b63d8d0a1b512b98fe466dffc7
xxc <282655181@qq.com>
2021-06-03 17:28:34 +0800

    增加PWM模拟电压输出，修复打印数值有负数

--- App/P0180_CD_SC92F732X/main.c
+++ App/P0180_CD_SC92F732X/main.c
@@ -20,6 +20,18 @@
 #include "modules.h"
 
 
+#define LEVEL_1()   	PWMDTY2 = 3
+#define LEVEL_2()   	PWMDTY2 = 11
+#define LEVEL_3()   	PWMDTY2 = 17
+#define LEVEL_4()   	PWMDTY2 = 23
+#define LEVEL_5()   	PWMDTY2 = 29
+#define LEVEL_6()   	PWMDTY2 = 35
+#define LEVEL_7()   	PWMDTY2 = 40
+#define LEVEL_8()   	PWMDTY2 = 46
+#define LEVEL_9()   	PWMDTY2 = 52
+#define LEVEL_10()  	PWMDTY2 = 58
+
+
 int32_t temperature, humidity;
 uint16_t CO2, TVOC;
 float absolutehumidity;
@@ -27,16 +39,33 @@ float absolutehumidity;
 
 void GPIO_Config(void)
 {    
-	P0CON = 0x08;		//p03输出
-	P2CON = 0x03;		//p20、p21输出
-
-	P2CON |= 0x00;		//p22、p23输入
-	P2PH |= 0x0C;		//p22、p23上拉输入
-
-
+	P0CON = 0xFF;		//p02输出
+	P0PH  = 0x00;
 	SC92F7321_NIO_Init();
 }
 
+void PWM_Init(void)
+{
+	PWMCON  = 0x3A;		//PWM输出到IO，PWM时钟为Fsys/4
+	PWMPRD  = 59;		//PWM周期=(59+1)*(1/6us)=10us	;
+	PWMCFG  = 0x00;		//PWM输出不反向,PWM2输出至P02
+ 	// PWMDTY0 = 30;		//PWM0的Duty = 30/60 =1/2
+	// PWMDTY1 = 15;       //PWM0的Duty = 15/60 =1/4
+	PWMDTY2 = 4;       //PWM0的Duty = 10/60 =1/6
+	PWMDTYA = 0x00;
+	PWMCON |= 0x80;     //开启PWM
+
+	// PWMCON  = 0x38;		//PWM输出到IO，PWM时钟为Fsys/4, 
+	// // PWMPRD  = 59;		//PWM周期=(59+1)*(1/6us)=10us
+	// // PWMCON  = 0x23;		//PWM2输出到IO，PWM时钟为Fsys/8
+	// PWMPRD  = 250;		//PWM周期=(59+1)*(1/256us)=10us
+	// PWMCFG  = 0x00;		//PWM输出不反向,PWM2输出至P02
+ 	// // PWMDTY0 = 30;		//PWM0的Duty = 30/60 =1/2
+	// // PWMDTY1 = 15;       //PWM0的Duty = 15/60 =1/4
+	// PWMDTY2 = 150;       //PWM0的Duty = 10/60 =1/6
+	// PWMDTYA = 0x00;
+	// PWMCON |= 0x80;     //开启PWM
+}
 
 void main(void)
 {
@@ -45,17 +74,63 @@ void main(void)
     sht3xDIS_Init();
     SGPC3_Init();
 
+	PWM_Init();
+
+	ENABLE_INT();
+
+
 
     while (1)
     {
 		SGPC3_measure(&CO2,&TVOC);
+
+		if (TVOC > 4001)
+		{
+			LEVEL_10();	
+		}
+		else if (TVOC > 3501)
+		{
+			LEVEL_9();	
+		}
+		else if (TVOC > 3001)
+		{
+			LEVEL_8();	
+		}		
+		else if (TVOC > 2501)
+		{
+			LEVEL_7();	
+		}		
+		else if (TVOC > 2001)
+		{
+			LEVEL_6();	
+		}
+		else if (TVOC > 1501)
+		{
+			LEVEL_5();	
+		}
+		else if (TVOC > 1001)
+		{
+			LEVEL_4();	
+		}
+		else if (TVOC > 501)
+		{
+			LEVEL_3();	
+		}
+		else if (TVOC > 251)
+		{
+			LEVEL_2();	
+		}
+		else
+		{
+			LEVEL_1();	
+		}
 
         if(sht3xDIS_RHMeasure(&temperature,&humidity))
         {
             absolutehumidity = get_absolute_humidity(temperature,humidity);
             sgp30_set_absolute_humidity(absolutehumidity);
             printf("temperature: %0.2f C, humidity: %0.2f RH.\r\n", temperature / 1000.0f, humidity / 1000.0f);
-            printf("CO2: %d ppm, TVOC: %d ppb.\r\n ", CO2 , TVOC);
+	        printf("CO2: %u ppm, TVOC: %u ppb.\r\n ", CO2 , TVOC);
         }
         else
         {
